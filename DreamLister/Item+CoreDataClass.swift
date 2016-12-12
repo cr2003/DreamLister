d887abfedc9e768b41f6fb3c9b84109b70010a88
cr2003 <crodrigo2003@gmail.com>
2016-12-12 16:07:23 +0100

    video 066 final

--- DreamLister/Item+CoreDataClass.swift
+++ DreamLister/Item+CoreDataClass.swift
@@ -12,4 +12,10 @@ import CoreData
 @objc(Item)
 public class Item: NSManagedObject {
 
+    public override func awakeFromInsert() {
+        
+        super.awakeFromInsert()
+        self.created = NSDate()
+    }
+    
 }
