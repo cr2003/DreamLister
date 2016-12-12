4502c394cddbcd6a4756974dbdeac3c04203719f
cr2003 <crodrigo2003@gmail.com>
2016-12-12 12:11:12 +0100

    video 065 final

---
+++ DreamLister/Image+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Image+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Admin on 12/12/2016.
+//  Copyright © 2016 50wise. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Image)
+public class Image: NSManagedObject {
+
+}
---
+++ DreamLister/ItemType+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  ItemType+CoreDataProperties.swift
+//  DreamLister
+//
+//  Created by Admin on 12/12/2016.
+//  Copyright © 2016 50wise. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+extension ItemType {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ItemType> {
+        return NSFetchRequest<ItemType>(entityName: "ItemType");
+    }
+
+    @NSManaged public var type: String?
+    @NSManaged public var toItem: Item?
+
+}
