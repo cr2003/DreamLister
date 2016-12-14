dce8f9d59457524aab0a95719a4e6afee1b5114a
cr2003 <crodrigo2003@gmail.com>
2016-12-14 11:24:00 +0100

    video 072 final

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -71,7 +71,9 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context!, sectionNameKeyPath: nil, cacheName: nil)
         
+        controller.delegate = self
         self.controller = controller
+        
         do {
             
             try controller.performFetch()
